045f7b49d2168af1f5f63e7854292d4f0efaba3d
mabus <m.buscaroli@outlook.com>
2021-05-26 16:58:04 +0100

    "Main View completed."

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,35 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Matteo on 26/05/2021.
+//
+
+import Foundation
+
+// Creating an Extension to Bundle to:
+// 1. Find the path to a file
+// 2. Load the file into an instance of Data
+// 3. Pass it through JSONDecoder
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        return loaded
+    }
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -4,15 +4,38 @@
 //
 //  Created by Matteo on 26/05/2021.
 //
+// Thanks to Paul Hudson and his Wonderful tutorials on Hacking with Swift
+// Credits for the data inside the JSON files and Pictures to Wikipedia:
+// https://creativecommons.org/licenses/by-sa/3.0
+//
 
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
